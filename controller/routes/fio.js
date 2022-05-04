4fd913858cb798fa0c1d35e52e9c4aff919b8fec
akyo8 <imirisa8@gmail.com>
2022-05-04 16:41:07 -0400

    implement uwrap action api

--- controller/routes/fio.js
+++ controller/routes/fio.js
@@ -7,4 +7,8 @@ route.get("/blocknumber/:net", checkAPIKey , (req, res) => fioCtrl.getBlockNumbe
 route.get("/health", checkAPIKey , (req, res) => fioCtrl.health(req,res));
 
 route.post("/setblocknumber/:net", checkAPIKey , (req, res) => fioCtrl.setBlockNumber(req,res));
+route.post("/setUnwrapAction/", checkAPIKey , (req, res) => fioCtrl.setUnwrapAction(req,res));
+route.get("/getUnwrapActionByTransaction/", checkAPIKey , (req, res) => fioCtrl.getUnwrapActionByTransaction(req,res));
+route.get("/getUnwrapActionByComplete/", checkAPIKey , (req, res) => fioCtrl.getUnwrapActionByComplete(req,res));
+
 export default route;
